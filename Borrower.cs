09c40913d17c6072d1df7460c6f7715d135fda4e
Veronica <161347755+hinA62@users.noreply.github.com>
2025-10-10 11:21:19 +0300

    Update Borrower.cs

--- Borrower.cs
+++ Borrower.cs
@@ -1,6 +1,9 @@
-﻿using Lab2;
+using Lab2;
 
 namespace Lab2
 {
-    public record Borrower(int Id, string Name, List<Book> BorrowedBooks);
+    public record Borrower(int Id, string Name, List<Book> BorrowedBooks)
+    {
+        public List<Book> myBooks = new List<Book>();
+    }
 }
